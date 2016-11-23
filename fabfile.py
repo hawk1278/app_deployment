238e20f53214130b2d5bee97379f5cbbbdef4d92
Rich O'Hara <richard@chimerasystems.net>
2016-11-23 13:21:47 -0500

    refactored fabfile and cleaned up code

--- fabfile.py
+++ fabfile.py
@@ -10,14 +10,14 @@ from fabric.contrib.files import upload_template
 from fabric.context_managers import cd, lcd
 from fabtools.user import create, exists
 from fabtools import require
-from fabtools.python import is_installed
+from fabtools.python import is_installed, virtualenv, virtualenv_exists
 #from fabtools.service import *
 from fabtools.files import is_link
 from fabtools.files import is_file, is_dir
 from fabtools.cron import add_task
 from fabtools.supervisor import start_process
 from fabtools.supervisor import stop_process
-#from fabtools.supervisor import reload_config
+from fabtools.supervisor import reload_config
 #import boto3
 #import os
 #import sys
@@ -34,13 +34,55 @@ app = "router_log_parser"
 app_dir = "/opt"
 user="logparser"
 
-# User tasks
+
 @task
-def add_user():
+def clone_repo(gitrepo="https://github.com/hawk1278/router_log_parser.git"):
     """
-    Create a user on host.
+    clone git repo set most recent pull as current.
     """
- 
+    with cd(app_dir):
+        sudo("git clone {0}".format(gitrepo))
+
+
+@task
+def setup_app():
+    """
+    Setup the application
+    """
+    if not exists(user):
+        puts(green("Creating user: {0}".format(user)))
+        create(user, comment="logparser applicaiton user", system=True, shell="/sbin/nologin")
+    if not is_dir("/var/log/{0}".format(app)):
+        with cd("/var/log"):
+            require.files.directory(app, owner=user, group=user, use_sudo=True)
+    with cd("{0}/{1}".format(app_dir, app)):
+        if not virtualenv_exists("{0}_env".format(app)):
+            sudo("virtualenv {0}_env".format(app))
+        if virtualenv_exists("{0}_env".format(app)):
+            with virtualenv("{0}_env".format(app)):
+                sudo("pip install -r requirements.txt",)
+
+@task
+def config_app(file_to_parse="/var/log/router.log"):
+    """
+    Config our application
+    """
+    app_venv = "{0}/{1}/{2}_env/bin/python".format(app_dir, app, app)
+    app_bin = "{0}/{1}/{2}.py".format(app_dir, app, app)
+    app_command = "{0} {1} -f {2}".format(app_venv, app_bin, file_to_parse)
+    logparser_context = {
+		      "logpath": "/var/log/router_log_parser/error.log",
+		      "errorlogpath": "/var/log/router_log_parser/parser.log",
+		      "file_to_parse": file_to_parse,
+              "app": app,
+              "command": app_command
+		      }
+    with cd("/etc/supervisor.d"):
+        if not is_file("router_log_parser.conf"):
+            upload_template("templates/router_log_parser.conf", \
+    "/etc/supervisor.d/router_log_parser.conf", \
+    context=logparser_context, mode=0700, use_sudo=True)
+            sudo("chown supervisor:root router_log_parser.conf")
 
 # Supervisor tasks
 @task
@@ -48,11 +90,9 @@ def setupsupervisor():
     """
     Setup supervisor on remote host.
     """
-    #require.python.package("supervisor", use_sudo=True)
     if not is_installed("supervisor"):
         puts(green("Installing supervisor."))
         sudo("pip install supervisor")
-    #install("supervisor", use_sudo=True)
     if not exists("supervisor"):
         puts(green("Creating supervisor user"))
         create("supervisor", comment="supervisord user", system=True)
@@ -79,43 +119,18 @@ def setupsupervisor():
         sudo("chkconfig --add supervisord")
         sudo("chkconfig --level 3 supervisord on")
         sudo("service supervisord start")
-
     with cd("/var/log"):
         if not is_file("supervisord.log"):
             puts(green("Creating supervisor log file"))
             sudo("touch supervisord.log && chown supervisor:root supervisord.log")
 
 
-
-@task
-def setup_app(file_to_parse="/var/log/router.log"):
-    """
-    Setup the application
-    """
-    logparser_context = {
-		      "logpath": "/var/log/router_log_parser/error.log",
-		      "errorlogpath": "/var/log/router_log_parser/parser.log",
-		      "file_to_parse": file_to_parse
-		      }
-    puts(green("Creating user: {0}".format(user)))
-    if not exists(user):
-        create(user, comment="logparser applicaiton user", system=True, shell="/sbin/nologin")
-              
-    if not is_file("/var/log/{0}".format(app)):
-        with cd("/var/log"):
-            require.files.directory(app, owner=user, group=user, use_sudo=True)
-    upload_template("templates/router_log_parser.conf", \
-    "/etc/supervisor.d/router_log_parser.conf", \
-    context=logparser_context[env], mode=0700, use_sudo=True)
-    require.python.virtualenv("{0}/{1}/{2}_env".format(app_dir, app, app))
-    with require.python.virtualenv("{0}/{1}/{2}_env".format(app_dir, app, app)):
-        require.python.requirements("{0}/{1}/requirements.txt".format(app_dir, app))
-
 @task
 def reload_conf():
     """
-    Reload supervisor config.abort
+    Reload supervisor config
     """
+    reload_config()
 
 
 @task
@@ -133,39 +148,16 @@ def end_process(process):
     """
     stop_process(process)
 
-# Application deployment tasks
-@task
-def deploy_config(local_config_dir, config_dir, config):
-    """
-Part of REST API deployment to upload specific supervisor configs
-    """
-    if is_file("{0}/{1}".format(config_dir, config)) is False:
-        with lcd(local_config_dir):
-            with cd(config_dir):
-                put("./{0}".format(config), "./{0}".format(config), use_sudo=True)
-                reload()
 
 @task
-def clone_repo(gitrepo="https://github.com/hawk1278/router_log_parser.git"):
-    """
-    clone git repo set most recent pull as current.
-    """
-    with cd(app_dir):
-        sudo("git clone {0} {1}".format(gitrepo, now))
-        if is_link('current'):
-            sudo("rm -f current")
-            sudo("ln -s {0} current".format(now))
-        else:
-            sudo("ln -s {0} current".format(now))
-            sudo("chown -R {0} current".format(user))
-
-
-@task
-def deploy(repo="https://github.com/hawk1278/router_log_parser.git", file_to_parse="/var/log/router.log"):
-
+def deploy(repo="https://github.com/hawk1278/router_log_parser.git", \
+file_to_parse="/var/log/router.log"):
     """
     Main task to deploy and configure application
     """
-    add_user()
     clone_repo(repo)
     setupsupervisor()
+    setup_app()
+    config_app()
+    #run_app()
+
